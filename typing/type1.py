f3cbb21144b56564f7b723825308751265323125
Robin Jenny <149656422+Robinjenny1@users.noreply.github.com>
2024-01-09 09:30:16 +0530

    Create type1.py

---
+++ typing/type1.py
@@ -0,0 +1,11 @@
+from typing import List,Optional
+
+Vector = List[float]
+
+def scale(scalar: float, vector: Vector) -> Vector:
+    return [scalar * num for num in vector]
+
+print(scale(2.0, [1.0,2.0,3.0]))
+
+
+

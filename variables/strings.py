46c26b0dfec65cd784738acd73db4535dd8e3641
Robin Jenny <149656422+Robinjenny1@users.noreply.github.com>
2024-01-09 09:52:21 +0530

    Create strings.py

---
+++ variables/strings.py
@@ -0,0 +1,7 @@
+import random
+# Split string method to convert string to list
+names_string = input("Give me everybody's names, separated by a comma. ")
+
+names = names_string.split(",")
+print(names_string)
+print(names)
